25c4de50c9a35d2bfd4e09811c1944dae62cd255
Sion Lee <35111239+sionara@users.noreply.github.com>
2024-05-02 17:54:50 -0400

    added target to resume anchor tag

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -61,6 +61,7 @@ const About = () => {
         <div className="mt-4">
           <a
             href={pdf}
+            target="_blank"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
             Download Resume
